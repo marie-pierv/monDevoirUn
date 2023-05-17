3cf2e89b849598c512fe7c0e4e334bdfa4c8f20e
mpvermette <marie-pier.vermette@hotmail.com>
2023-05-17 19:17:52 -0400

    test commit

---
+++ src/components/card-body/CardBody.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./CardBody.css"
+
+const CardBody = () => {
+  return (
+    <div className='card-body'>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, veritatis? Delectus, atque accusantium perspiciatis deserunt quisquam natus! Aut, soluta ipsam.
+        </p>
+    </div>
+  );
+};
+
+export default CardBody
